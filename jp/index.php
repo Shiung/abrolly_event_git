5de0c9f547ec5ebef335ab0d64ebb2b4b3665e1e
Jack <shiungchenyu@gmail.com>
2019-06-04 11:17:29 +0800

    Update index.php
    切換追蹤 gtm 碼

--- jp/index.php
+++ jp/index.php
@@ -84,6 +84,14 @@ switch ($language){
 	<!-- short-icon -->
 	<!-- <link rel="Shortcut Icon" type="image/x-icon" href=""> -->
 
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-PXWTCKC');</script>
+	<!-- End Google Tag Manager -->
+
 	<style>
 		@font-face{
 			font-family: "Hiragino Sans";
@@ -120,6 +128,10 @@ switch ($language){
 	</style>
   </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PXWTCKC"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <header>
 	<div class="nav-logo"><img src="../img/logo.png" class="img-fluid"></div>
 	<div class="container-fluid">
@@ -517,35 +529,6 @@ switch ($language){
 	</div>
 	
 </footer>
-
-<!-- Facebook Pixel Code -->
-	<script>
-	  !function(f,b,e,v,n,t,s)
-	  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-	  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-	  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-	  n.queue=[];t=b.createElement(e);t.async=!0;
-	  t.src=v;s=b.getElementsByTagName(e)[0];
-	  s.parentNode.insertBefore(t,s)}(window, document,'script',
-	  'https://connect.facebook.net/en_US/fbevents.js');
-	  fbq('init', '1892773974325375');
-	  fbq('track', 'PageView');
-	</script>
-	<noscript><img height="1" width="1" style="display:none"
-	  src="https://www.facebook.com/tr?id=1892773974325375&ev=PageView&noscript=1"
-	/></noscript>
-	<!-- End Facebook Pixel Code -->
-
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-97477245-3"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-97477245-3');
-</script>
-
 
 </body>  
 </html>
